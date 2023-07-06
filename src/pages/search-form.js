c2a5f2afca9796ac57e45f764e4711b6112425ec
agnes cameron <agnesfcameron@gmail.com>
2023-07-06 12:30:46 +0100

    done quicksearch

--- src/pages/search-form.js
+++ src/pages/search-form.js
@@ -1,21 +1,64 @@
 /* src/components/search-form.js */
 import React, { useState, useRef } from "react"
 import { navigate } from "@reach/router"
+import { Link, graphql, useStaticQuery } from "gatsby"
+import { Index } from "lunr"
+
 const SearchForm = ({ initialQuery = "" }) => {
   // Create a piece of state, and initialize it to initialQuery
   // query will hold the current value of the state,
   // and setQuery will let us change it
   const [query, setQuery] = useState(initialQuery)
+  const [results, setResults] = React.useState([]);
+
+  const data = useStaticQuery( graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+      LunrIndex
+    }
+`)
   
+  // LunrIndex is available via page query
+  const { store } = data.LunrIndex
+  // Lunr in action here
+  const index = Index.load(data.LunrIndex.index)
   // We need to get reference to the search input element
   const inputEl = useRef(null)
 
   // On input change use the current value of the input field (e.target.value)
   // to update the state's query value
-  const handleChange = e => {
-    setQuery(e.target.value)
+  const quickSearch = event => {
+    // const q = e.target.value
+    let q = event.target.value.slice(-1) === " " ? event.target.value : event.target.value + '*';
+    setQuery(event.target.value)
+    let res = []
+    try {
+      // Search is a lunr method
+      res = index.search(q, {
+          fields: {
+              title: {boost: 10},
+              description: {boost: 5}
+          }
+        }).map(({ ref }) => {
+        // Map search results to an array of {slug, title, excerpt} objects
+        return {
+          slug: ref,
+          ...store[ref],
+        }
+      })
+      console.log('got results', results)
+      setResults(res)
+    } catch (error) {
+      console.log(error)
+    }
+
   }
-  
+
+
   // When the form is submitted navigate to /search
   // with a query q paramenter equal to the value within the input search
   const handleSubmit = e => {
@@ -25,6 +68,7 @@ const SearchForm = ({ initialQuery = "" }) => {
     navigate(`/search?q=${q}`)
   }
   return (
+    <div>
     <form role="search" onSubmit={handleSubmit}>
       <label htmlFor="search-input" style={{ display: "block" }}>
         Search for:
@@ -35,10 +79,18 @@ const SearchForm = ({ initialQuery = "" }) => {
         type="search"
         value={query}
         placeholder="e.g. duck"
-        onChange={handleChange}
+        onChange={quickSearch}
       />
       <button type="submit">Go</button>
     </form>
+      <div>
+        { results.length > 0 && <div><b>Quick search results:</b> 
+          { results.filter( (item, i) => i < 5 ).map( 
+            (result, j) => <li key={j}> {result.title} </li> 
+            )}</div>}
+      </div>
+      </div>
   )
 }
 export default SearchForm
+
