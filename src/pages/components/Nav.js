4fc12aae4f0ba18d3badf8c7f6d6b15d570318fc
Agnes Cameron <agnesfcameron@gmail.com>
2023-10-27 10:49:20 +0100

    added header image, changed layout

--- src/pages/components/Nav.js
+++ src/pages/components/Nav.js
@@ -2,17 +2,25 @@ import React from "react"
 import { Link } from "gatsby"
 
 const Nav = () => {
-	return( 
+	return(
+		<>
+		<header>
+			<div class="headerContainer">
+				<a href="/"><img src="/assets/open_inno_index.png" /></a>
+			</div>
+		</header>
+
 		<div class="navContainer">
 			<div class="nav">
 				<a href="/">home</a>
 				<a href="/about">about</a>
-				<Link to="/search/">advanced search</Link>
+				<Link to="/search/">search</Link>
 				<Link to="/datasets/">datasets</Link>
 				<Link to="/tools/">tools</Link>
 				<Link to="/guides/">guides</Link>
 			</div>
 		</div>
+		</>
 	)
 }
 
--- src/pages/components/layout.js
+++ src/pages/components/layout.js
@@ -5,7 +5,7 @@ export default function Layout({ children }) {
   return (
     <main>
       <Nav />
-      <div style={{ margin: `0 auto`, maxWidth: 650, padding: `0 1rem` }}>
+      <div className="pageContainer">
         {children}
       </div>
     </main>
